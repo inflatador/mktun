9389dcc772909f47ed165d0d0cdfbffbefcc8f42
Brian King <bking@wikimedia.org>
2023-01-12 16:37:14 -0600

    jumphost renamed to remotehost, for accuracy's sake

--- mktun.sh
+++ mktun.sh
@@ -4,13 +4,13 @@
 LOCALPORT="$1"
 REMOTEIP="$2"
 REMOTEPORT="$3"
-JUMPHOST="$4"
+REMOTEHOST="$4"
 
 set -euo pipefail
 
 if [[ $# -eq 0 ]] ; then
     printf "%s\n" "usage: mktun LOCALPORT REMOTEIP REMOTEPORT JUMPHOST"
-    exit 0
+    exit 1
 fi
 printf "%s\n" "Creating a tunnel on local port ${1}"
 ssh -Cfo ExitOnForwardFailure=yes -NL ${LOCALPORT}:${REMOTEIP}:${REMOTEPORT} ${USER}@${JUMPHOST}
